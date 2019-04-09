0d54f431596a964e66816eb51277327225518b43
Marcos Brito <mbrito@itba.edu.ar>
2019-04-09 16:42:48 -0300

    Desarrollo de PowerManager driver y funcion FE_Sort() en FileExplorer

--- Mp3Player/source/PowerManager.h
+++ Mp3Player/source/PowerManager.h
@@ -1,6 +1,14 @@
 #ifndef POWERMANAGER_H_
 #define POWERMANAGER_H_
 
+#define PM_ATTACHED_MODULE_COUNT 15
 
+typedef void (*PM_deinitCallback)(void);
+
+void PM_EnterLowPowerMode();
+// Returns true if recovered from Low Power mode. False if normal reset.
+_Bool PM_Recover();
+
+void PM_AttachDeinitFunction(PM_deinitCallback fun);
 
 #endif /* POWERMANAGER_H_ */
