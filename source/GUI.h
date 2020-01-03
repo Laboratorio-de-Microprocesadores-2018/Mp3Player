a10af8fc3534872e651fd3e74a529b5843f08d64
Tobías Lifschitz <tlifschitz@itba.edu.ar>
2020-01-03 02:06:00 -0300

    WIP GUI

--- source/GUI.h
+++ source/GUI.h
@@ -11,50 +11,33 @@
 /////////////////////////////////////////////////////////////////////////////////
 //                             Included header files                           //
 /////////////////////////////////////////////////////////////////////////////////
+#if defined(_WIN64) || defined(_WIN32)
 
+#else
 #include "GILI9341.h"
 #include "Input.h"
-
+#endif
 
 /////////////////////////////////////////////////////////////////////////////////
 //                       Constants and macro definitions                       //
 /////////////////////////////////////////////////////////////////////////////////
-#define GUI_TOP_HEADER_HOR_SIZE	LV_HOR_RES
-#define GUI_TOP_HEADER_VER_SIZE	LV_VER_RES * 0.1
 
-#define GUI_TAB_VIEW_HOR_SIZE	LV_HOR_RES
-#define GUI_TAB_VIEW_VER_SIZE	LV_VER_RES * 0.9
+
 
 /////////////////////////////////////////////////////////////////////////////////
 //                    Enumerations, structures and typedefs                    //
 /////////////////////////////////////////////////////////////////////////////////
-typedef void(*GUI_hal_init_cb_t)(lv_indev_t ** indev);
 
-typedef enum {HOME_TAB_ID = 0, SEARCH_TAB_ID, LIBRARY_TAB_ID} GUI_tab_id_t;
-typedef struct
-{
 
-	lv_disp_drv_t dispDrv;
-	lv_disp_t * disp;
-	lv_indev_t * encIndev, * keyPadIndev;
-	lv_theme_t * theme;
+typedef enum {
+	MUSIC_SCREEN,
+	BROWSER_SCREEN,
+	SETTINGS_SCREEN,
+	POWER_SCREEN,
+	MENU_SCREEN,
+	SCREEN_NUM
+}ScreenID_t;
 
-	lv_obj_t * screen;
-
-	lv_obj_t * header;
-	lv_obj_t * headerTimeTxt, * headerBatteryTxt;
-
-	lv_obj_t * tabView;
-	lv_obj_t * homeTab, * searchTab, * libraryTab;
-
-	lv_group_t * tabGroup;
-	GUI_tab_id_t currTabId;
-	uint16_t tabIdCount;
-
-	lv_group_t * libraryGroup;
-	lv_obj_t * libraryList;
-
-}GUI;
 
 /////////////////////////////////////////////////////////////////////////////////
 //                         Global function prototypes                          //
@@ -65,8 +48,21 @@ typedef struct
  * @param Hardware abstraction layer function pointer, to be executed by the GUI Initialization. This is send a parameter, because the "lv_indev_t obj" (The input devices to be registered by the GUI are defined inside the scope of GUI.c)
  */
 void GUI_Init(void);
-void GUI_UpdateHeader(void);
-lv_disp_drv_t * GUI_GetDispDrv(void);
+
+/**
+ *
+ */
+void GUI_Create(void);
+
+/*
+ *
+ */
+void GUI_Task(void);
+
+/*
+ *
+ */
+void GUI_FlushReady(void);
 
 /**********************
 *      MACROS
