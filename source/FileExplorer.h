4e2b9aee073fd43536e7c438669fce32a1d53688
Tobías Lifschitz <tlifschitz@itba.edu.ar>
2020-01-03 02:15:08 -0300

    WIP Visual simulation

--- source/FileExplorer.h
+++ source/FileExplorer.h
@@ -9,13 +9,26 @@
 #ifndef FILEEXPLORER_H_
 #define FILEEXPLORER_H_
 
+
+
+
+#if defined(_WIN64) || defined(_WIN32)
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef int32_t status_t;
+
+#else
+#include <stdbool.h>
 #include "fsl_common.h"
 #include "ff.h"
-#include "stdbool.h"
+#endif
+
 
 #define MAX_FILES_PER_DIR 255
 #define SORTING_END_CHAR 0
 
+#if !(defined(_WIN64) || defined(_WIN32))
 #define FE_EOF(fp) 		 f_eof(fp)
 #define FE_Error(fp) 	 f_error(fp)
 #define FE_Size(fp) 	 f_size(fp)
@@ -23,6 +36,7 @@
 #define FE_RewindDir(dp) f_rewinddir(dp)
 #define FE_RmDir(path) 	 f_rmdir(path)
 #define FE_Unmount(path) f_unmount(path)
+#endif
 
 typedef enum
 {
@@ -32,10 +46,10 @@ typedef enum
 
 
 typedef enum {
-	NONE,
-	ABC,
-	SIZE
-} FE_FILE_SORT_TYPE;
+	SORTE_NONE,
+	SORT_ALPHABETIC,
+	SORT_SIZE
+} FE_SortCriteria_t;
 
 
 /*
@@ -70,39 +84,56 @@ status_t FE_unmountDrive(FE_drive drive);
  */
 status_t FE_SetCurrDrive(FE_drive drive);
 
+
+#if defined(_WIN64) || defined(_WIN32)
+	/*
+	FE_DirN
+	FE_OpenFile
+	FE_ReadFile
+	FE_CloseFile
+	FE_OpenDir
+	FE_OpenFileN
+	*/
+#else
+
 /*
  *
  */
 status_t FE_DirN(const char* path, uint16_t* n, FILINFO* content);
 
 
-static inline FRESULT FE_OpenFile( FIL* fp, const TCHAR* path, BYTE mode )
+
+static inline FRESULT FE_OpenFile(FIL* fp, const TCHAR* path, BYTE mode)
 {
-	return f_open(fp,path,mode);
+	return f_open(fp, path, mode);
 }
 
-static inline FRESULT FE_ReadFile (FIL* fp,  void* buff,  UINT btr,  UINT* br)
+static inline FRESULT FE_ReadFile(FIL* fp, void* buff, UINT btr, UINT* br)
 {
-	return f_read(fp,buff,btr,br);
+	return f_read(fp, buff, btr, br);
 }
 
-static inline FRESULT FE_CloseFile (FIL* fp )
+static inline FRESULT FE_CloseFile(FIL* fp)
 {
 	return f_close(fp);
 }
 
-static inline FRESULT FE_OpenDir (DIR* dp, const char* path)
+static inline FRESULT FE_OpenDir(DIR* dp, const char* path)
 {
-	return f_opendir(dp,path);
+	return f_opendir(dp, path);
 }
 
-bool FE_DriveStatus(FE_drive drive);
 
 /**
  * @brief Open the nth file in the dir which matches the pattern
  */
 
-FRESULT FE_OpenFileN(const char * path, FIL* fp,FILINFO *fileInfo, BYTE mode, uint8_t n, const char * pattern);
+FRESULT FE_OpenFileN(const char* path, FIL* fp, FILINFO* fileInfo, BYTE mode, uint8_t n, const char* pattern);
+
+#endif
+
+bool FE_DriveStatus(FE_drive drive);
+
 
 /**
  *
@@ -113,7 +144,7 @@ uint8_t FE_CountFilesMatching(const char * path, const char * pattern);
 /**
  * @brief Sorts the files with a given extension from path to indexArray following a sort criteria.
  */
-uint8_t FE_Sort(FE_FILE_SORT_TYPE sort ,const char * path, const char * pattern, uint8_t * indexArray);
+uint8_t FE_Sort(FE_SortCriteria_t sort ,const char * path, const char * pattern, uint8_t * indexArray);
 
 
 #endif /* FILEEXPLORER_H_ */
