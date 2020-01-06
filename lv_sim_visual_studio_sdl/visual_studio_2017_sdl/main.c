febb9a38d1efcbe1ac87679161c38ea2a58ff9be
Tobías Lifschitz <tlifschitz@itba.edu.ar>
2020-01-06 05:07:22 -0300

    Visual Project Update

--- lv_sim_visual_studio_sdl/visual_studio_2017_sdl/main.c
+++ lv_sim_visual_studio_sdl/visual_studio_2017_sdl/main.c
@@ -1,149 +1,31 @@
-/**
-* @file main
-*
-*/
 
 /*********************
 *      INCLUDES
 *********************/
-#include <stdlib.h>
-#include <Windows.h>
+//#include <stdlib.h>
+
 #include <SDL.h>
 #include <SDL_mutex.h>
 
 #include "GUI.h"
+#include "FileExplorer.h"
 #include "MP3Player.h"
-
-
-/*********************
-*      DEFINES
-*********************/
-
-/**********************
-*      TYPEDEFS
-**********************/
-
-/**********************
-*  STATIC PROTOTYPES
-**********************/
-
-static int tick_thread(void *data);
-void mp3Player_thread(void* data);
-/**********************
-*  STATIC VARIABLES
-**********************/
-
-
-/**********************
-*      MACROS
-**********************/
-
-/**********************
-*   GLOBAL FUNCTIONS
-**********************/
 
 int main(int argc, char** argv)
 {
-    /*Initialize GUI*/
 	GUI_Init();
 	GUI_Create();
-	
+	FE_Init();
 	MP3_Init();
 
-	//demo_create();
-	/* Tick init.
-	* You have to call 'lv_tick_inc()' in every milliseconds
-	* Create an SDL thread to do this*/
-	SDL_CreateThread(tick_thread, "tick", NULL);
-  
-	/* Thread to simulate MP3 module*/
-	SDL_CreateThread(mp3Player_thread, "mp3Player", NULL);
-
-
-    while (1) {
-        /* Periodically call the lv_task handler.
-        * It could be done in a timer interrupt or an OS task too.*/
+    while (GUI_PowerOffRequest()==false)
+	{
 		GUI_Task();
 		MP3_Task();
-        Sleep(10);       /*Just to let the system breathe */
+        SDL_Delay(5);       /*Just to let the system breathe */
     }
 
     return 0;
 }
 
-/**********************
-*   STATIC FUNCTIONS
-**********************/
 
-
-int elapsed = 0;
-int duration = 145;
-status_t status = IDLE;
-
-//
-//void MP3_Play(char* path)
-//{
-//	elapsed = 0;
-//	status = PLAYING;
-//}
-//
-//void MP3_Pause()
-//{
-//	status = PAUSE;
-//}
-//
-//void MP3_GetStatus()
-//{
-//	return status;
-//}
-//int MP3_GetTrackProgress()
-//{
-//	return elapsed*100/duration;
-//}
-//
-//int MP3_GetPlaytime()
-//{
-//	return elapsed;
-//}
-//
-//int MP3_GetDuration()
-//{
-//	return duration;
-//}
-
-void mp3Player_thread(void* data)
-{
-	SDL_mutex* mutex = SDL_CreateMutex();
-
-	while (1)
-	{
-		switch (status)
-		{
-		case IDLE:
-			break;
-		case PLAYING:
-			if (elapsed < duration)
-				elapsed++;
-			else
-				status = IDLE;
-			break;
-		case PAUSE:
-			break;
-		}
-		SDL_Delay(1000);
-	}
-}
-/**
-* A task to measure the elapsed time for LittlevGL
-* @param data unused
-* @return never return
-*/
-static int tick_thread(void *data)
-{
-    while (1) {
-        lv_tick_inc(5);
-        SDL_Delay(5);   /*Sleep for 1 millisecond*/
-    }
-
-    return 0;
-}
